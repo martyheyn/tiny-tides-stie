4587b8bd28687e428b187eb72ae091130297f99c
Marty Heyn <heynmarty@gmai.com>
2024-12-12 18:06:33 -0500

    updated vercelServerless

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,13 @@ import { defineConfig } from 'astro/config'
 
 import tailwind from '@astrojs/tailwind'
 
-import vercel from '@astrojs/vercel/serverless'
+import vercelServerless from '@astrojs/vercel/serverless'
 
-import svelte from '@astrojs/svelte';
+import svelte from '@astrojs/svelte'
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), svelte()],
   output: 'server',
-  adapter: vercel(),
+  adapter: vercelServerless(),
 })
