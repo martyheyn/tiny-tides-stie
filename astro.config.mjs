7fc2aa3f79e4ec7791498e3eb8257de46f845d4f
Marty Heyn <heynmarty@gmai.com>
2025-04-17 09:04:07 -0400

    add partytown to help run google ad tag

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,24 @@
 // @ts-check
 import { defineConfig } from 'astro/config'
-
 import tailwind from '@astrojs/tailwind'
-
 import vercelServerless from '@astrojs/vercel/serverless'
-
 import svelte from '@astrojs/svelte'
-
 import sitemap from '@astrojs/sitemap'
+import partytown from '@astrojs/partytown'
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.tinytidestherapy.com/',
-  integrations: [tailwind(), svelte(), sitemap()],
+  integrations: [
+    tailwind(),
+    svelte(),
+    sitemap(),
+    partytown({
+      config: {
+        forward: ['dataLayer.push'],
+      },
+    }),
+  ],
   output: 'server',
   adapter: vercelServerless(),
 })
