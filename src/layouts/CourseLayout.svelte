00b1a7934be1b9269be26eba515520f29d5d5080
Marty Heyn <heynmarty@gmai.com>
2025-10-20 07:43:40 -0400

    merged in main branch

---
+++ src/layouts/CourseLayout.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  import Sidebar from '../components/courses/Sidebar.svelte'
+
+  let { chapters } = $props()
+
+  let sidebarOpen = $state(true)
+</script>
+
+<div class="flex flex-col md:flex-row gap-4 transition-all ease-in-out duration-300">
+  <div class="hidden md:block border-r border-black/20 ">
+    <Sidebar bind:value={sidebarOpen} {chapters} />
+  </div>
+
+  <div class={`transition-all ease-in-out duration-300 flex-1`}>
+     <slot sidebarOpen={sidebarOpen} />
+  </div>
+
+  <div class="block md:hidden">
+    <Sidebar bind:value={sidebarOpen} {chapters} />
+  </div>
+</div>
