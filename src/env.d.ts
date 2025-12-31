436efd7a74f6d9a33445d7888873d86435c69a87
Marty Heyn <heynmarty@gmai.com>
2025-12-31 07:58:20 -0500

    use Astro Image element

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,11 +1,3 @@
 /// <reference path="../.astro/types.d.ts" />
 
-declare global {
-  function gtag(
-    command: 'config' | 'event',
-    targetId: string,
-    params?: Record<string, any>,
-  ): void
-}
-
 export {}
